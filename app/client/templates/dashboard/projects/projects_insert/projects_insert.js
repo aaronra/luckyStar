4258a8194529600554f317104859aecac4ba2c5f
Jr Reyes <jr.evilboss@gmail.com>
2016-03-22 14:26:43 +0800

    Added new Template Project Insert

---
+++ app/client/templates/dashboard/projects/projects_insert/projects_insert.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* ProjectsInsert: Event Handlers */
+/*****************************************************************************/
+Template.ProjectsInsert.events({
+});
+
+/*****************************************************************************/
+/* ProjectsInsert: Helpers */
+/*****************************************************************************/
+Template.ProjectsInsert.helpers({
+});
+
+/*****************************************************************************/
+/* ProjectsInsert: Lifecycle Hooks */
+/*****************************************************************************/
+Template.ProjectsInsert.created = function () {
+};
+
+Template.ProjectsInsert.rendered = function () {
+};
+
+Template.ProjectsInsert.destroyed = function () {
+};
