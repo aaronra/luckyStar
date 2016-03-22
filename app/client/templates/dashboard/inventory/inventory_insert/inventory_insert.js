a165e71d7003928ade0bedfde067a45bda5baea3
Jr Reyes <jr.evilboss@gmail.com>
2016-03-22 13:42:03 +0800

    Added new template add  inventory  template

---
+++ app/client/templates/dashboard/inventory/inventory_insert/inventory_insert.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* InventoryInsert: Event Handlers */
+/*****************************************************************************/
+Template.InventoryInsert.events({
+});
+
+/*****************************************************************************/
+/* InventoryInsert: Helpers */
+/*****************************************************************************/
+Template.InventoryInsert.helpers({
+});
+
+/*****************************************************************************/
+/* InventoryInsert: Lifecycle Hooks */
+/*****************************************************************************/
+Template.InventoryInsert.created = function () {
+};
+
+Template.InventoryInsert.rendered = function () {
+};
+
+Template.InventoryInsert.destroyed = function () {
+};
