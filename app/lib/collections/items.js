41ff07e81a59b950b28b35e3028f89fa44e0415b
Jr Reyes <jr.evilboss@gmail.com>
2016-03-22 13:41:18 +0800

    Created new Item collection

--- app/lib/collections/items.js
+++ app/lib/collections/items.js
@@ -1,5 +1,21 @@
 Items = new Mongo.Collection('items');
-
+Items.attachSchema(new SimpleSchema({
+  name: {
+    type: String,
+    unique: true
+  },
+  quantity: {
+    type: Number
+  },
+  description: {
+    type: String,
+    label: 'Description'
+  },
+  price: {
+    type: Number,
+    optional: false
+  }
+}));
 Items.helpers({
 
 });
